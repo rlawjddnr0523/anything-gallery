21a0d3b5ad90e9dc63a2ce3e507789681f1065c7
김정욱 <yeoback07@icloud.com>
2022-05-28 21:51:04 +0900

    sdfsdf

--- desktop/menu.php
+++ desktop/menu.php
@@ -2,13 +2,16 @@
     <div class="items">
         <ul>
             <li>
-                <a href="index.php">메인화면</a>
-            </li>
-            <li>
                 <a href="gallery.php">갤러리</a>
             </li>
             <li>
                 <a href="mgallery.php">마이너갤러리</a>
+            </li>
+            <li>
+                <a href="blog.php">블로그</a>
+            </li>
+            <li>
+                <a href="mblog.php">마이너블로그</a>
             </li>
         </ul>
     </div>
