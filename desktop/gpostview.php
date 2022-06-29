76c0b4d8d0a18e4c9523a429fe30654c92067f1e
김정욱 <yeoback07@icloud.com>
2022-06-29 19:55:14 +0900

    ㄴㅇㄹ
    ㄴㅇㄹ

--- desktop/gpostview.php
+++ desktop/gpostview.php
@@ -2,6 +2,12 @@
 require "top.php";
 $mysqli = mysqli_connect("localhost", "kjwook0523","Kimmark4167*","kjwook0523");
 $num = $_GET['num'];
+$query1 = "SELECT * FROM edm_b where bid = '$num'";
+$result1 = mysqli_query($mysqli, $query1);
+$fetch = mysqli_fetch_array($result1);
+$view = $fetch['view'] + 1;
+$query2 = "UPDATE edm_b SET view = '$view' WHERE bid = '$num'";
+$result2 = mysqli_query($mysqli, $query2);
 $query = "SELECT * FROM edm_b where bid = '$num'";
 $result = mysqli_query($mysqli, $query);
 while($row = mysqli_fetch_array($result)){
@@ -12,7 +18,7 @@ while($row = mysqli_fetch_array($result)){
         <div class="pv-2"><h3><?php echo $row['bcontents']; ?></h3></div>
         <div class="pv-3"><h6>작성자 : <?php echo $row['bwriter']; ?></h6></div>
         <div class="pv-4"><h6>작성일 : <?php echo $row['bdate']; ?></h6></div>
-        <div class="pv-6"><h6>조회수 : <?php echo $row['view']; ?></h6></div>
+        <div class="pv-6"><h6>조회수 : <?php echo $row['view'];?>회</h6></div>
     </div>
     <?php
 }
--- desktop/index.php
+++ desktop/index.php
@@ -1,6 +1,7 @@
 <?php
     require "top.php";
 ?>
+
 </body>
 <footer>
 
