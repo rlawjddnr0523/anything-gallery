a5274d8276933303d9a095b38978cc39b3d9bebf
김정욱 <yeoback07@icloud.com>
2022-06-29 18:19:20 +0900

    sdf
    sdf

--- desktop/gpostview.php
+++ desktop/gpostview.php
@@ -1,3 +1,4 @@
 <?php
 require "top.php";
+
 ?>
--- desktop/gupload.php
+++ desktop/gupload.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+?>
+<meta charset="utf-8">
+<?php
 $mysqli=mysqli_connect("localhost", "kjwook0523","Kimmark4167*","kjwook0523");
 
 $title = $_POST['title'];
@@ -7,6 +11,16 @@ $writer = $_SESSION['master1'];
 $writed = date("Y-m-d H:i:sa");
 $gallery = $_GET['name'];
 $stats = "y";
-if(isset($_POST['file']) == true){
-
+$query = "insert into edm_b (btitle, bcontents, bwriter, bdate,gtitle,bstats, bfile) values ('$title', '$contents', '$writer', '$writed','$gallery', '$stats', '$file')";
+$result = mysqli_query($mysqli, $query);
+if($result) {
+    ?><script>
+        alert("업로드에 성공하였습니다.");
+        location.href="galleryview.php?name=<?php echo $_GET['name']; ?>";
+    </script><?php
+} else {
+    ?><script>
+        alert("업로드에 실패하였습니다.");
+        history.back();
+</script><?php
 }
